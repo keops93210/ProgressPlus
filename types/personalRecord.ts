1272e0695258dff85da2fc227f589e88c1f0dd1b
keops93210 <emeran.andy@gmail.com>
2026-08-04 10:21:34 +0200

    Create personalRecord.ts

---
+++ types/personalRecord.ts
@@ -0,0 +1,15 @@
+export interface PersonalRecord {
+  id: string;
+
+  user_id: string;
+
+  exercise_id: string;
+
+  weight: number;
+
+  reps: number;
+
+  estimated_1rm: number;
+
+  created_at: string;
+}
