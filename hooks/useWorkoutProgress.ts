4a263a935a49d24b444dc17893cacab4c3c5fed2
keops93210 <emeran.andy@gmail.com>
2026-08-07 11:52:49 +0200

    Update useWorkoutProgress.ts

--- hooks/useWorkoutProgress.ts
+++ hooks/useWorkoutProgress.ts
@@ -1,1 +1,28 @@
+import { useMemo } from "react";
 
+export function useWorkoutProgress(
+  currentExerciseIndex: number,
+  totalExercises: number,
+  currentSet: number,
+  totalSets: number
+) {
+  const exerciseProgress = useMemo(() => {
+    if (totalSets === 0) return 0;
+
+    return (currentSet / totalSets) * 100;
+  }, [currentSet, totalSets]);
+
+  const workoutProgress = useMemo(() => {
+    if (totalExercises === 0) return 0;
+
+    return (
+      ((currentExerciseIndex + 1) / totalExercises) *
+      100
+    );
+  }, [currentExerciseIndex, totalExercises]);
+
+  return {
+    exerciseProgress,
+    workoutProgress,
+  };
+}
