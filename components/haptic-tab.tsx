51ff888e469435f95910c173e8cef818f010fe0b
Andy Emeran <emeran.andy@gmail.com>
2026-08-09 20:18:43 +0200

    fix: finalize Expo 56 and Live Activity setup

--- components/haptic-tab.tsx
+++ components/haptic-tab.tsx
@@ -1,17 +1,20 @@
-import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
-import { PlatformPressable } from '@react-navigation/elements';
-import * as Haptics from 'expo-haptics';
+﻿import { Pressable } from "react-native";
+import * as Haptics from "expo-haptics";
 
-export function HapticTab(props: BottomTabBarButtonProps) {
+export function HapticTab(
+  props: React.ComponentProps<typeof Pressable>
+) {
   return (
-    <PlatformPressable
+    <Pressable
       {...props}
-      onPressIn={(ev) => {
-        if (process.env.EXPO_OS === 'ios') {
-          // Add a soft haptic feedback when pressing down on the tabs.
-          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+      onPressIn={(event) => {
+        if (process.env.EXPO_OS === "ios") {
+          void Haptics.impactAsync(
+            Haptics.ImpactFeedbackStyle.Light
+          );
         }
-        props.onPressIn?.(ev);
+
+        props.onPressIn?.(event);
       }}
     />
   );
